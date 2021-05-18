706be91b9da8df283253d5b98ea02ef2409f03f2
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2021-05-18 11:15:01 +0300

    refactor(tcp-server): fix clippy

--- tcp-server/src/main.rs
+++ tcp-server/src/main.rs
@@ -1,18 +1,15 @@
-use std::borrow::BorrowMut;
-use std::collections::vec_deque::Iter;
 use std::collections::VecDeque;
 use std::io;
-use std::ops::Deref;
 use std::sync::Arc;
 use std::time::Duration;
 
-use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::Mutex;
 use tokio::task::JoinHandle;
 use tokio::time;
 
-use redisish::{parse, Command};
+use redisish::{Command, parse};
 
 struct VecDequeMailbox {
     data: Mutex<VecDeque<String>>,
@@ -37,9 +34,9 @@ impl VecDequeMailbox {
 impl VecDequeMailbox {
     // TODO how to return Box<dyn Mailbox>?
     fn new() -> Box<VecDequeMailbox> {
-        return Box::new(VecDequeMailbox {
+        Box::new(VecDequeMailbox {
             data: Mutex::new(VecDeque::new()),
-        });
+        })
     }
 }
 
@@ -47,13 +44,15 @@ impl VecDequeMailbox {
 /// * how to work with `dyn Mailbox ` in multithreaded environment?
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    tokio::join!(spawn_monitoring_thread(), spawn_tcp_listener(),);
+    let (res1, res2) = tokio::join!(spawn_monitoring_thread(), spawn_tcp_listener());
+    res1?;
+    res2?;
     Ok(())
 }
 
 /// Spawns TcpListener thread, connects to 127.0.0.1:8080 and spawns thread per connection
 fn spawn_tcp_listener() -> JoinHandle<()> {
-    return tokio::spawn(async {
+    tokio::spawn(async {
         let mailbox = Arc::new(VecDequeMailbox::new());
         let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
 
@@ -61,10 +60,10 @@ fn spawn_tcp_listener() -> JoinHandle<()> {
             let next = listener.accept().await.unwrap();
             let mailbox = mailbox.clone();
             tokio::spawn(async {
-                handle_client(BufReader::new(next.0), mailbox).await;
+                handle_client(BufReader::new(next.0), mailbox).await.unwrap();
             });
         }
-    });
+    })
 }
 
 /// TODO how can I pass the mailbox, preferably just mailbox: Mailbox or at least mailbox: Arc<Box<dyn Mailbox>>?
@@ -87,8 +86,8 @@ async fn handle_client(
             Ok(Command::Retrieve) => {
                 tcp_stream
                     .write_all(mailbox.list_emails().await.as_ref())
-                    .await;
-                tcp_stream.write_all("\n".as_ref()).await;
+                    .await?;
+                tcp_stream.write_all("\n".as_ref()).await?;
             }
             Err(err) => {
                 println!("Client error: {}", err);
@@ -104,14 +103,15 @@ fn spawn_monitoring_thread() -> JoinHandle<()> {
     tokio::spawn(async {
         // TODO replace with retry
         time::sleep(Duration::from_millis(1000)).await;
-        let mut client = TcpStream::connect("127.0.0.1:8080").await.unwrap();
+        let client = TcpStream::connect("127.0.0.1:8080").await.unwrap();
         let mut client = BufReader::new(client);
         loop {
             client
                 .write_all(Command::Retrieve.as_string().as_ref())
-                .await;
+                .await
+                .unwrap();
             let mut str = String::new();
-            client.read_line(&mut str).await;
+            client.read_line(&mut str).await.unwrap();
             println!("Mailbox content: {}", str.trim_end());
             time::sleep(Duration::from_millis(5000)).await;
         }
